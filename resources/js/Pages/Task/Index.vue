d6f1b3a66b025a817a93c7d9a47353053dd4665e
aleprosli <aliff.rosli96@gmail.com>
2023-05-02 12:31:01 +0800

    fix: form name

--- resources/js/Pages/Task/Index.vue
+++ resources/js/Pages/Task/Index.vue
@@ -5,7 +5,7 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { useForm, Head } from '@inertiajs/vue3';
  
 const form = useForm({
-    message: '',
+    name: '',
 });
 </script>
  
@@ -16,7 +16,7 @@ const form = useForm({
         <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
             <form @submit.prevent="form.post(route('tasks.store'), { onSuccess: () => form.reset() })">
                 <textarea
-                    v-model="form.message"
+                    v-model="form.name"
                     placeholder="What's on your mind?"
                     class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                 ></textarea>
